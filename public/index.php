d8c96f1d856ff43ea1afa3c0576b47f33f36a548
adhamStudy <adhm000111000111@gmail.com>
2025-01-07 19:32:43 +0300

    after refactor folder structure

--- public/index.php
+++ public/index.php
@@ -1,15 +1,15 @@
 <?php
 // Include the helpers file
 require '../helpers.php';
-require basePath('Router.php');
-require basePath('Database.php');
+require basePath('Framework/Router.php');
+require basePath('Framework/Database.php');
 
 // instaiiationg the router
 $router = new Router();
 // get routes
 $routes = require basePath('routes.php');
 // get current URI and HTTP method
-$uri = $_SERVER['REQUEST_URI'];
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = $_SERVER['REQUEST_METHOD'];
 // Route the request
 $router->route($uri, $method);
