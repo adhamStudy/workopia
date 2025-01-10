a374b1f7839ab9b016a80803b694c59512244819
adhamStudy <adhm000111000111@gmail.com>
2025-01-10 18:44:39 +0300

    add icon to title of the website

--- App/views/partials/head.php
+++ App/views/partials/head.php
@@ -5,8 +5,13 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- Favicon (Direct URL) -->
+    <link rel="icon" type="image/png" href="https://img.icons8.com/fluency/48/permanent-job.png">
+    <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Custom CSS -->
     <link rel="stylesheet" href="/css/style.css" />
+    <!-- Page Title -->
     <title>Workopia</title>
 </head>
 
